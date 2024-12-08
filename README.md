624ac0d4956a90f485e4bd50405d754905fd21c8
Bharat6191 <bharatsolanke111@gmail.com>
2024-12-08 14:35:34 +0530

    non_repeating string

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Python Coding Questions
 
 
-https://chatgpt.com/c/67549f7b-c788-800c-aabf-4bc11414ecf4
+<!-- https://chatgpt.com/c/67549f7b-c788-800c-aabf-4bc11414ecf4 -->
+
--- String/Non_repeating.py
+++ String/Non_repeating.py
@@ -1,11 +1,26 @@
 # Find the first non-repeating character in a string.
 
 
+def approach_2(string):
+  for i in string:
+    if string.count(i)==1:
+      return i
+  return False
 
 def approach_1(string):
-  pass
+  dic={}
+  for i in string:
+    if i in dic:
+      dic[i]=dic[i]+1
+    else:
+      dic[i]=1
+  for i in dic:
+    if dic[i]==1:
+      return i
+  return False
 
 if __name__=="__main__":
   string="AAAABBBBBBCCCCCDDDE"
   print(approach_1(string))
+  print(approach_2(string))
   
